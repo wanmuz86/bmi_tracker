49df2141943f4fa19a86c0238433372fb6272179
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-05-02 22:47:04 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:bmitracker/screens/input_page.dart';
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
